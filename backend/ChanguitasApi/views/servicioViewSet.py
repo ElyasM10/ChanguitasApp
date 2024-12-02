5ba108e5033b8570c0eec712d2ccaada1d36455c
majo-fflores <mariajosefflores@gmail.com>
2024-12-02 16:06:36 -0300

    BACK: actualizacion del servicio para agregar mas de un dia y horario cuando estoy creando un servicio

--- backend/ChanguitasApi/views/servicioViewSet.py
+++ backend/ChanguitasApi/views/servicioViewSet.py
@@ -9,12 +9,20 @@ class ServicioViewSet(viewsets.ModelViewSet):
     serializer_class = ServicioSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            # Guarda el nuevo servicio en la base de datos
-            self.perform_create(serializer)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # Si recibimos una lista, manejamos múltiples registros
+        if isinstance(request.data, list):
+            serializer = self.get_serializer(data=request.data, many=True)
+            if serializer.is_valid():
+                self.perform_create(serializer)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            # Manejo para un único registro
+            serializer = self.get_serializer(data=request.data)
+            if serializer.is_valid():
+                self.perform_create(serializer)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)

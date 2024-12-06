d289bdb40a5896cdce2a8492700940ebf93b0954
Elias <elias63856@gmail.com>
2024-12-05 21:07:05 -0300

    .

--- screens/API_URL.js
+++ screens/API_URL.js
@@ -1,6 +1,6 @@
-//const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'http://127.0.0.1:8000';
 
 //Para android studio la ip  y  python manage.py runserver 0.0.0.0:8000
-const API_URL ='http://10.0.2.2:8000/';
+//const API_URL ='http://10.0.2.2:8000/';
 
 export default API_URL;
